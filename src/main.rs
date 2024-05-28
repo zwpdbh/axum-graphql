5f731b387cba6ac8ea45b3281a963716c7bb32fe
zhaowei <zhaowei@microsoft.com>
2024-05-28 16:45:16 +0800

    > adding graphQL

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use axum::routing::get;
 use axum::{Router, Server};
+use routes::health;
+
+mod routes;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new();
+    let app = Router::new().route("/health", get(health));
 
     Server::bind(&"0.0.0.0:8000".parse().unwrap())
         .serve(app.into_make_service())
