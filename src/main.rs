a579b680c2f0e1f5e96a9c1f7ef79179d95e6413
zwpdbh <zw>
2024-05-28 21:26:44 +0800

    > time to add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
+use std::future::ready;
+
 use crate::model::QueryRoot;
+use crate::observability::metrics::{create_prometheus_recorder, track_metrics};
 use crate::routes::{graphql_handler, graphql_playground, health};
 use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use axum::middleware;
 use axum::{extract::Extension, routing::get, Router, Server};
 
 mod model;
+mod observability;
 mod routes;
 
 #[tokio::main]
 async fn main() {
     let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+    let prometheus_recorder = create_prometheus_recorder();
 
     let app = Router::new()
         .route("/", get(graphql_playground).post(graphql_handler))
         .route("/health", get(health))
+        .route("/metrics", get(move || ready(prometheus_recorder.render())))
+        .route_layer(middleware::from_fn(track_metrics))
         .layer(Extension(schema));
 
     Server::bind(&"0.0.0.0:8000".parse().unwrap())
